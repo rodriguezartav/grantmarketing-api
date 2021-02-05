bb3dba7f7d5fb915bdc6258fe43c5b15932062bc
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-05 12:58:23 -0600

    fc

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
-  const items = await req.knex.table("customers").select();
-  res.json({ data: items });
+  res.json({
+    "X-Hasura-User-Id": "25",
+    "X-Hasura-Role": "user",
+    "X-Hasura-Is-Owner": "true",
+    "X-Hasura-Custom": "custom value",
+  });
+});
+
+router.post("/", async function (req, res, next) {
+  res.json({
+    "X-Hasura-User-Id": "25",
+    "X-Hasura-Role": "user",
+    "X-Hasura-Is-Owner": "true",
+    "X-Hasura-Custom": "custom value",
+  });
 });
 
 module.exports = router;
