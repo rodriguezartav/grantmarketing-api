2ca48f91e1f97573dbb597ba8a02b2c70bf483b3
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-13 21:14:46 -0600

    customer

--- workers/process1.js
+++ workers/process1.js
@@ -42,33 +42,35 @@ setInterval(async () => {
         (item) => (integrationMap[item.provider_name] = item)
       );
 
+      let tryError;
       try {
         let { stdout, stderr, error } = await execFile("node", [
           `./scripts/${job.script_location}/${job.script_name}.js`,
           JSON.stringify(integrationMap),
           job.customer_id,
         ]);
+        tryError = error;
       } catch (e) {
-        error = e;
+        tryError = e;
       }
 
       await knex.table("executions").insert({
         schedule_id: job.schedule_id,
-        result: error ? 1 : 0,
+        result: tryError ? 1 : 0,
       });
 
       await knex.table("jobs").delete().where("id", job.id);
-      if (error) {
+      if (tryError) {
         await knex.table("script_logs").insert({
           script_id: job.script_id,
           job_id: job.id,
           log: {
             stderr: stderr || "",
-            error: { stack: error.stack, message: err.stack },
+            error: { stack: tryError.stack, message: tryError.stack },
           },
         });
 
-        await sms(error.message.substring(0, 35), job.admin_phone);
+        await sms(tryError.message.substring(0, 35), job.admin_phone);
       } else
         await knex
           .table("schedules")
