37c13051bc0b0ddc9afea3fa7b3efce97e8c2306
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-24 18:21:35 -0600

    fix contactpssf

--- scripts/helpers/runner.js
+++ scripts/helpers/runner.js
@@ -5,19 +5,20 @@ const moment = require("moment");
 
 module.exports = async function (scriptPath) {
   console.log("START");
-  if (!scriptPath) scriptPath = process.env.SCRIPT;
-  const integrationMap = JSON.parse(process.env.INTEGRATION_MAP);
-  console.log(scriptPath);
-  const script = require("../" + scriptPath);
-
-  const timeStart = moment();
-  console.log(timeStart);
-  setInterval(() => {
-    const now = moment();
-    if (now.diff(timeStart, "seconds") > 60 * 5 * 1000)
-      throw new Error("Timeout");
-  }, 1000);
   try {
+    if (!scriptPath) scriptPath = process.env.SCRIPT;
+    const integrationMap = JSON.parse(process.env.INTEGRATION_MAP);
+    console.log(scriptPath);
+    const script = require("../" + scriptPath);
+
+    const timeStart = moment();
+    console.log(timeStart);
+    setInterval(() => {
+      const now = moment();
+      if (now.diff(timeStart, "seconds") > 60 * 5 * 1000)
+        throw new Error("Timeout");
+    }, 1000);
+
     console.log("SCRIPT_START");
     await script(integrationMap);
     console.log("END");
