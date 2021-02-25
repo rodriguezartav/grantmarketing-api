4f8b74bb736d2eaeebf6a619a3f0565ae0d9ff10
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-25 06:34:03 -0600

    provider nqme

--- scripts/google/ads.js
+++ scripts/google/ads.js
@@ -8,13 +8,6 @@ const moment = require("moment");
 
 module.exports = async function Run(integrationMap) {
   try {
-    const googleTokens = await Knex()
-      .table("integration_tokens")
-      .select()
-      .join("providers", "providers.id", "integration_tokens.provider_id")
-      .where("providers.name", "google")
-      .first();
-
     const googleIntegration = integrationMap["google"];
 
     const client = new GoogleAdsApi({
@@ -23,9 +16,10 @@ module.exports = async function Run(integrationMap) {
       developer_token: process.env.GOOGLE_ADS_DEVELOPER_TOKEN,
     });
 
+    console.log(googleIntegration);
     const customer = client.Customer({
       customer_id: googleIntegration.application_id,
-      refresh_token: google.auth_token,
+      refresh_token: googleIntegration.auth_token,
     });
 
     const campaigns = await customer.report({
@@ -46,12 +40,10 @@ module.exports = async function Run(integrationMap) {
     });
 
     //  await trx.commit();
-    await knex.destroy();
     process.exit(0);
   } catch (e) {
     //  if (trx) await trx.rollback();
     console.log(e);
-    await knex.destroy();
     throw e;
   }
 };
