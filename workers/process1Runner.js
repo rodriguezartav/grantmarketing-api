993d6656c3337d518eb7e598bb4c821e8508f925
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-24 18:45:54 -0600

    fix contactpssf

--- workers/process1Runner.js
+++ workers/process1Runner.js
@@ -1,13 +1,12 @@
-
+const util = require("util");
 const execFile = util.promisify(require("child_process").execFile);
 
 try {
-    setInterval(()=>{
-        let { stdout, stderr, error } = await execFile("node", [
-            `./workers/process1.js`,
-          ]);
-    },60000)
-  
+  setInterval(async () => {
+    let { stdout, stderr, error } = await execFile("node", [
+      `./workers/process1.js`,
+    ]);
+  }, 60000);
 } catch (e) {
   console.log("CRITICAL_ERROR");
   console.error(e);
