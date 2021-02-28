6cf5288cc1be868760437dafc9cbe9601063cf88
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-02-28 15:44:31 -0600

    scriptptions

--- workers/jobCreator.js
+++ workers/jobCreator.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const moment = require("moment");
 
+const Slack = require("../helpers/slack");
 const Knex = require("../helpers/knex");
 
 setInterval(async () => {
   try {
     const knex = Knex();
+
+    const slack = await Slack();
 
     let schedules = await knex.table("schedules").select();
     for (let index = 0; index < schedules.length; index++) {
@@ -29,6 +32,29 @@ setInterval(async () => {
           script_id: schedule.script_id,
           customer_id: schedule.customer_id,
         });
+      } else {
+        const jobs = await knex
+          .table("jobs")
+          .select(
+            "jobs.*",
+            "scripts.name as script_name",
+            "customers.name as customer_name"
+          )
+          .join("customers", "customers.id", "jobs.customer_id")
+          .join("scripts", "scripts.id", "jobs.script_id")
+          .whereIn("status", ["working"])
+          .where("created_at", ">", moment().add(-10, minutes))
+          .where("schedule_id", schedule.id);
+
+        if (jobs.length > 0) {
+          await slack.chat.postMessage({
+            text: `Some jobs seem to be stock. ${jobs.map(
+              (item) =>
+                `${item.script_name} ${item.customer_name} ${item.created_at}`
+            )}`,
+            channel: slack.generalChannelId,
+          });
+        }
       }
     }
   } catch (e) {
