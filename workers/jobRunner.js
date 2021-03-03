3dc98321b68b53c17390b3c15fe97a31f09ed762
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-03-03 08:10:44 -0600

    add heroku wb

--- workers/jobRunner.js
+++ workers/jobRunner.js
@@ -83,7 +83,6 @@ async function JobRunner(knex) {
           `API_EVENT:::HEROKU_RUNNER:::START:::${JSON.stringify({
             job_id: job.id,
             time: moment().valueOf(),
-            herokuScript_name: dynoRes.name,
             integrationMap: Object.keys(integrationMap),
             script: script,
             scriptOptions: scriptOptions,
