74244c8ca56fb70fe5ca720d3cbed1f65d84c573
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-03-03 07:01:26 -0600

    add heroku wb

--- routes/integrations/heroku.js
+++ routes/integrations/heroku.js
@@ -9,7 +9,7 @@ const { TenNinetyNineContact } = require("xero-node");
 
 router.post("/webhook", async function (req, res, next) {
   const { exit_status, command, name } = req.body.data;
-  const action = req.body.sta;
+  const action = req.body.data.action;
   console.log(action, exit_status, command.split("job_id=")[1], name);
   return res.json({});
 });
