9e8f40d457a42b9fb9b180d12edaab9ea77fe67b
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-03-12 20:00:34 -0600

    fox error  for job 13489

--- scripts/rodco/contactsSfToCustomerIO.js
+++ scripts/rodco/contactsSfToCustomerIO.js
@@ -1,4 +1,5 @@
 let CIO = require("../../helpers/customerio");
+const request = require("superagent");
 
 const moment = require("moment");
 const { sfConn, bulk, query } = require("../../helpers/sf");
@@ -7,6 +8,10 @@ let knex;
 module.exports = async function Run(integrationMap) {
   try {
     const conn = await sfConn(integrationMap["salesforce"]);
+
+    const cioAuth = new Buffer.from(
+      `${integrationMap["customerio"].client_id}:${integrationMap["customerio"].client_secret}`
+    ).toString("base64");
 
     const cio = CIO(integrationMap["customerio"]);
 
@@ -17,22 +22,25 @@ module.exports = async function Run(integrationMap) {
 
     for (let index = 0; index < contacts.length; index++) {
       const contact = contacts[index];
-      console.log(contact.id);
-      const res = await cio.identify(contact.id, {
-        email: contact.email,
-        created_at: moment(contact.createddate).unix(),
-        name: contact.name,
-        mobile: contact.mobilephone,
-        brands: contact.description,
-        balance: contact.account__r ? contact.account__r.saldo__c : 0,
-        role: contact.title,
-        segment: contact.department,
-        customer_since: contact.createddate,
-        companyName: contact.account__r ? contact.account__r.name : null,
-        companyId: contact.account__r ? contact.account__r.id : null,
-      });
 
-      await sleep(350);
+      const res = await request
+        .put(`https://track.customer.io/api/v1/customers/${contact.id}`)
+        .set("Authorization", `Basic ${cioAuth}`)
+        .send({
+          email: contact.email,
+          created_at: moment(contact.createddate).unix(),
+          name: contact.name,
+          mobile: contact.mobilephone,
+          brands: contact.description,
+          balance: contact.account__r ? contact.account__r.saldo__c : 0,
+          role: contact.title,
+          segment: contact.department,
+          customer_since: contact.createddate,
+          companyName: contact.account__r ? contact.account__r.name : null,
+          companyId: contact.account__r ? contact.account__r.id : null,
+        });
+
+      await sleep(10);
     }
   } catch (e) {
     console.log(e);
