edc22b958923df31bae35b8ef95b5fcbc82e508b
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-03-15 16:45:12 -0600

    add vpi route for customer pg api

--- scripts/alpaca/monitor.js
+++ scripts/alpaca/monitor.js
@@ -4,18 +4,18 @@ const Knex = require("../../helpers/knex_pg");
 const Alpaca = require("../../helpers/alpaca");
 const moment = require("moment");
 
-const knex = async function Run(integrationMap, users, scriptOptions) {
+async function Run(integrationMap, users, scriptOptions) {
   const pgString = integrationMap["postgres"];
   const alpacaKeys = integrationMap["alpaca"];
   const knex = Knex(pgString);
 
-  const symbols = await knex.table("symbols").select();
-  Alpaca.getBars(
+  const symbols = await knex.table("stocks").select();
+  await Alpaca.getBars(
     alpacaKeys,
     symbols.map((item) => item.symbol)
   );
 
   return true;
-};
+}
 
 module.exports = Run;
