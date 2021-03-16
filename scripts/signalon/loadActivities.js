e4133e97d4d4b0d7de0deb2c3ea1170613b2ff28
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-03-16 16:19:41 -0600

    fix resolve map

--- scripts/signalon/loadActivities.js
+++ scripts/signalon/loadActivities.js
@@ -20,9 +20,9 @@ module.exports = async function Run(integrationMap) {
     const filter = {
       createdAt: {
         startAt: "2021-03-01T00:00:00Z",
-        endAt: "2021-03-13T00:00:00Z",
-        activityTypeIds: [3, 6, 7, 8, 9, 10, 11, 27, 41, 40, 39, 48, 111, 112],
+        endAt: "2021-03-02T00:00:00Z",
       },
+      activityTypeIds: [3, 6, 7, 8, 9, 10, 11, 27, 41, 40, 39, 48, 111, 112],
     };
 
     const activities = await Marketo.batch(
