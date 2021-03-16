f245b890c1a167930767d2300bc83844848061ba
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-03-15 19:30:28 -0600

    add vpi route for customer pg api

--- helpers/alpaca.js
+++ helpers/alpaca.js
@@ -4,27 +4,31 @@ const moment = require("moment");
 
 function Alpaca(integration) {}
 
-Alpaca.getBars = async function (integration, symbols) {
+Alpaca.getBars = async function (
+  integration,
+  symbols,
+  groupBy = "1Min",
+  daysAgo = -2
+) {
   let symbolsWithBars = [];
 
   for (let index = 0; index < symbols.length; index++) {
     const symbol = symbols[index];
-    const bars = await Alpaca._getBars(integration, symbol);
+    const bars = await Alpaca._getBars(integration, symbol, groupBy, daysAgo);
     symbolsWithBars.push({ symbol, bars });
   }
   return symbolsWithBars;
 };
 
-Alpaca._getBars = async function (integration, symbol) {
+Alpaca._getBars = async function (integration, symbol, groupBy, daysAgo) {
   try {
     const response = await request
-      .get(`https://data.alpaca.markets/v1/bars/1Min`)
+      .get(`https://data.alpaca.markets/v1/bars/${groupBy}`)
       .query({
         symbols: symbol,
-        after: moment().add(-2, "days").format("YYYY-MM-DDT00:00:00Z"),
+        after: moment().add(daysAgo, "days").format("YYYY-MM-DDT00:00:00Z"),
         limit: 1000,
       })
-
       .set("APCA-API-KEY-ID", integration.client_id)
       .set("APCA-API-SECRET-KEY", integration.client_secret);
 
