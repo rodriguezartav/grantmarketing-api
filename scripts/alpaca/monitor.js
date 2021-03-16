996c86bf30994347f69ebc0f10a03b97e3ffed4b
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-03-16 14:31:56 -0600

    unix

--- scripts/alpaca/monitor.js
+++ scripts/alpaca/monitor.js
@@ -34,9 +34,39 @@ async function Run(integrationMap, users, scriptOptions) {
       .where("symbol", stock.symbol)
       .where("time", ">", moment().add(-1, "days").startOf("day").unix())
       .orderBy("time", "DESC");
+
+    const plMax = await knex
+      .table("unrealized_profits")
+      .max("plpc")
+      .where("symbol", stock.symbol)
+      .where("time", ">", moment().add(-1, "days").startOf("day").unix());
+
+    if (position.unrealized_plpc < -0.01)
+      await sms(`${stock.symbol} is dropping to -1%`, "+50684191862");
+    else if (position.unrealized_plpc < -0.015)
+      await sms(`${stock.symbol} dropped to -1.5%`, "+50684191862");
+    else if (position.unrealized_plpc > 0.05)
+      await sms(
+        `${stock.symbol} is reaching to ${formatPl(position.unrealized_plpc)}`,
+        "+50684191862"
+      );
+    else if (position.unrealized_plpc < plMax[0].max)
+      await sms(
+        `${stock.symbol} is dropping from Max of ${formatPl(plMax[0].max)}`,
+        "+50684191862"
+      );
+    else if (position.unrealized_plpc > plMax[0].max)
+      await sms(
+        `${stock.symbol} is climbing from Max of ${formatPl(plMax[0].max)}`,
+        "+50684191862"
+      );
   }
 
   return true;
 }
 
 module.exports = Run;
+
+function formatPl(pl) {
+  return parseInt(pl * 1000) / 10 + "%";
+}
